fb0fd20272f33f7ea7d4acaa75f09cda757cadfe
HandsomeJeck <115385928+RDR2Blackwater@users.noreply.github.com>
2024-02-21 23:50:21 +0800

    Update train.py

--- train.py
+++ train.py
@@ -301,7 +301,7 @@ class SimCLR_trainer(object):
             if avg_valid_acc > best_acc:
                 print(f"Best model found at epoch {epoch}, saving model")
                 self.accelerator.save(self.accelerator.unwrap_model(finetune_model).state_dict(),
-                                      f"./ResNet50_{self.conf.width_multiplier}/_finetune_best.ckpt")
+                                      f"./ResNet50_{self.conf.width_multiplier}/{self.conf.epochs} epochs SimCLR_finetune_best.ckpt")
                 best_acc = avg_valid_acc
 
             # Release Connection to wandb
@@ -316,7 +316,7 @@ class SimCLR_trainer(object):
         model_best = SimCLR_series.SimCLR_v1(self.conf.backbone, self.conf.width_multiplier, self.conf.features)
         model_best.backbone.fc = nn.Linear(2048 * self.conf.width_multiplier if self.conf.backbone == 'simclr_resnet50' else 1, 10)
 
-        finetune_best = torch.load("./ResNet50_{self.conf.width_multiplier}/finetune_best.ckpt")
+        finetune_best = torch.load("./ResNet50_{self.conf.width_multiplier}/{self.conf.epochs} epochs SimCLR_finetune_best.ckpt")
         model_best.load_state_dict(finetune_best)
 
         model_best.eval()
